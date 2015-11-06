3a206c25feef0df7757d2f5fa05495018622b716
Michel Cadennes <michel.cadennes@sens-commun.fr>
2015-11-06 15:28:46 +0100

    RSS PHP

---
+++ 2. Introduction à XML/xml-rss.php
@@ -0,0 +1,46 @@
+<?php
+
+$flux = array('http://rss.lemonde.fr/c/205/f/3060/index.rss', 'http://liberation.fr.feedsportal.com/c/32268/fe.ed/rss.liberation.fr/rss/58/');
+
+$texte = $_GET['texte'];
+// Créer un doc XML
+$doc = new DOMDocument();
+
+foreach ($flux as $f) {
+	// Récupération du flux
+	$doc->load($f);
+	// Création un chemin
+	$xpath = new DOMXPath($doc);
+	$query = '//channel/item';
+	// Exécution de la requête
+	$entries = $xpath->query($query);
+	// Stockage des items dans un tableau
+	$selection = array();
+	foreach ($entries as $entry) {
+		$query_titre = 'titre';
+		$query_image = 'enclosure';
+		$query_lien = 'link';
+		$query_description = 'description';
+
+		$titre = $xpath->query($query_titre, $entry);
+		$lien = $xpath->query($query_lien, $entry);
+		$image = $xpath->query($query_image, $entry);
+		$desc = $xpath->query($query_description, $entry);
+
+	if (strstr($desc->nodeValue, $texte) !== false) {
+		$selection['titre'] = $titre->nodeValue;
+		$selection['lien'] = $lien->nodeValue;
+		$selection['image'] = $image->getAttribute('url');
+	}
+}
+
+?>
+<h1>Dépêches contenant : <?php $texte ?></h1>
+<div>
+<?php
+foreach($selection as $artiste) {
+	echo "<div><img src-'".$selection['image']."'/></div>";
+	echo "<div><a href='".$selection['lien']."'>".$artiste['titre']."</a></div>";
+}
+?>
+</div>
