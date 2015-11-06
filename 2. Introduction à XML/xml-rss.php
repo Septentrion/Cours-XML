440138af61245429e8ba056e3b42a1928f79beab
Michel Cadennes <michel.cadennes@sens-commun.fr>
2015-11-06 16:51:49 +0100

    RSS PHP

--- 2. Introduction à XML/xml-rss.php
+++ 2. Introduction à XML/xml-rss.php
@@ -18,35 +18,36 @@ foreach ($flux as $f) {
 	// Stockage des items dans un tableau
 	$selection = array();
 	foreach ($entries as $entry) {
-		$query_titre = 'titre';
+		$query_titre = 'title';
 		$query_image = 'enclosure';
 		$query_lien = 'link';
 		$query_description = 'description';
-
 		$titre = $xpath->query($query_titre, $entry);
 		$lien = $xpath->query($query_lien, $entry);
 		$image = $xpath->query($query_image, $entry);
 		$desc = $xpath->query($query_description, $entry);
 
-	if (strstr($desc->nodeValue, $texte) !== false) {
-		$selection['titre'] = $titre->nodeValue;
-		$selection['lien'] = $lien->nodeValue;
-		$selection['image'] = $image->getAttribute('url');
+	if (strstr($desc->item(0)->nodeValue, $texte) !== false) {
+		$item = array();
+		$item['titre'] = $titre->item(0)->nodeValue;
+		$item['lien'] = $lien->item(0)->nodeValue;
+		$item['image'] = $image->item(0)->getAttribute('url');
+		$selection[] = $item;
 
-		$node = $newdoc->importNode($entry, true);
+		$node = $target->importNode($entry, true);
 		$target->appendChild($node);
 	}
 }
 
-target->save('veille.xml');
+$target->save('veille.xml');
 
 ?>
 <h1>Dépêches contenant : <?php $texte ?></h1>
 <div>
 <?php
-foreach($selection as $artiste) {
-	echo "<div><img src-'".$selection['image']."'/></div>";
-	echo "<div><a href='".$selection['lien']."'>".$artiste['titre']."</a></div>";
+foreach($selection as $depeche) {
+	echo "<div><img src-'".$depeche['image']."'/></div>";
+	echo "<div><a href='".$depeche['lien']."'>".$depeche['titre']."</a></div>";
 }
 ?>
 </div>
